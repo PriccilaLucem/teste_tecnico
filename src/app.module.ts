e99609333227f50d205b404cc993d13ced55b8b5
Priccila Lucem <priccilalucem@gmail.com>
2024-04-23 11:08:21 -0300

    connecting to db

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { CatsController } from './controllers/user_controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: false, //mudar isso depoiss
+    }),
+  ],
+  controllers: [CatsController],
+  providers: [AppService],
+})
+export class AppModule {}
